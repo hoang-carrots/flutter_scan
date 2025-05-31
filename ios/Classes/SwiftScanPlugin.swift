0c57911a1f4df0e24527c4060911518f4953cf90
Hoang <hoang@carrots.so>
2025-05-31 22:16:20 +0700

    Update SwiftScanPlugin.swift

--- ios/Classes/SwiftScanPlugin.swift
+++ ios/Classes/SwiftScanPlugin.swift
@@ -12,18 +12,24 @@ public class SwiftScanPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method=="getPlatformVersion" {
-      result("iOS " + UIDevice.current.systemVersion)
-    } else if call.method=="parse" {
-      let path = call.arguments as! String;
-      if let features = self.detectQRCode(UIImage.init(contentsOfFile: path)), !features.isEmpty {
-        let data = features.first as! CIQRCodeFeature
-        result(data.messageString);
+  if call.method == "getPlatformVersion" {
+    result("iOS " + UIDevice.current.systemVersion)
+  } else if call.method == "parse" {
+    let path = call.arguments as! String
+    if let features = self.detectQRCode(UIImage(contentsOfFile: path)), !features.isEmpty {
+      if let data = features.first as? CIQRCodeFeature, let message = data.messageString {
+        result([
+          "value": message,
+          "isBarcode": false
+        ])
       } else {
-        self.detectBarCode(UIImage.init(contentsOfFile: path), result: result)
+        result(nil)
       }
+    } else {
+      self.detectBarCode(UIImage(contentsOfFile: path), result: result)
     }
   }
+}
   
   private func detectQRCode(_ image: UIImage?) -> [CIFeature]? {
     if let image = image, let ciImage = CIImage.init(image: image){
@@ -43,34 +49,49 @@ public class SwiftScanPlugin: NSObject, FlutterPlugin {
   }
   
   private func detectBarCode(_ image: UIImage?, result: @escaping FlutterResult) {
-    if let image = image, let ciImage = CIImage.init(image: image), #available(iOS 11.0, *) {
-      var requestHandler: VNImageRequestHandler;
-      if ciImage.properties.keys.contains((kCGImagePropertyOrientation as String)) {
-        requestHandler = VNImageRequestHandler(ciImage: ciImage, orientation: CGImagePropertyOrientation(rawValue: ciImage.properties[(kCGImagePropertyOrientation as String)] as! UInt32) ?? .up, options: [:])
+    if let image = image, let ciImage = CIImage(image: image), #available(iOS 11.0, *) {
+      var requestHandler: VNImageRequestHandler
+      if ciImage.properties.keys.contains(kCGImagePropertyOrientation as String) {
+        requestHandler = VNImageRequestHandler(
+          ciImage: ciImage,
+          orientation: CGImagePropertyOrientation(
+            rawValue: ciImage.properties[kCGImagePropertyOrientation as String] as? UInt32 ?? 1
+          ) ?? .up,
+          options: [:]
+        )
       } else {
         requestHandler = VNImageRequestHandler(ciImage: ciImage, orientation: .up, options: [:])
       }
-      let request = VNDetectBarcodesRequest { (request,error) in
-        var res: String? = nil;
-        if let observations = request.results as? [VNBarcodeObservation], !observations.isEmpty {
-          let data: VNBarcodeObservation = observations.first!;
-          res = data.payloadStringValue;
+  
+      let request = VNDetectBarcodesRequest { (request, error) in
+        var payload: String? = nil
+        if let observations = request.results as? [VNBarcodeObservation], let first = observations.first {
+          payload = first.payloadStringValue
         }
+  
         DispatchQueue.main.async {
-          result(res);
+          if let payload = payload {
+            result([
+              "value": payload,
+              "isBarcode": true
+            ])
+          } else {
+            result(nil)
+          }
         }
       }
+  
       DispatchQueue.global(qos: .background).async {
-        do{
+        do {
           try requestHandler.perform([request])
         } catch {
           DispatchQueue.main.async {
-            result(nil);
+            result(nil)
           }
         }
       }
     } else {
-      result(nil);
+      result(nil)
     }
   }
 }
